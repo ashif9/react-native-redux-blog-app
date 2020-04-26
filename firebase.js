b7673256dca90ff16d4799bfd5afcbba05628628
Ashif Sadique <ashif.sadique9@gmail.com>
2020-04-26 17:16:46 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from "firebase"
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDZvgw6rwtxIYRJrErDyeBmg9AtLyOT7f4",
-    authDomain: "blog-app-e8773.firebaseapp.com",
-    databaseURL: "https://blog-app-e8773.firebaseio.com",
-    projectId: "blog-app-e8773",
-    storageBucket: "blog-app-e8773.appspot.com",
-    messagingSenderId: "538548410266",
-    appId: "1:538548410266:web:9a1b2d150e0834b504cc23",
-    measurementId: "G-EBFTMFHW1J"
+    apiKey: "YOUR_API",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATA_BASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_API_ID",
+    measurementId: "YOUR_ID"
 };
 // Initialize Firebase
 export default firebase.initializeApp(firebaseConfig);
